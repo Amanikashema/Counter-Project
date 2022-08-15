87e619c38cb4d4949c7f1f22d43ae6cf64343708
Amani Kashema <akashema@dvt.co.za>
2022-08-15 23:24:23 +0200

    Counter Project files

---
+++ app.js
@@ -0,0 +1,37 @@
+let count = 0;
+
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
+
+
+btns.forEach(function(btn){
+    console.log(btn)
+    btn.addEventListener('click',function(e){
+        const styles = e.currentTarget.classList
+        if(styles.contains('decrease')){
+            count--;
+            // value.textContent = count;
+        }
+        else if(styles.contains('increase')){
+            count++;
+            // value.textContent = count;
+        }
+
+        else if(styles.contains('reset')){
+            count = 0 
+        }
+
+        value.textContent = count;
+
+        if (count > 0){
+            value.style.color = 'green'
+        }
+        if (count < 0){
+            value.style.color = 'red'
+        }
+
+        if (count == 0){
+            value.style.color = '#222'
+        }
+    })
+})
